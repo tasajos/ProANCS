2e9e9074817762689ee8634fff102207f16d31c8
carlos azcarraga <tasajos@gmail.com>
2023-02-05 00:42:38 -0400

    create structure and tips

--- ProyN/src/app/app.module.ts
+++ ProyN/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PrincipalComponent } from './Components/principal/principal.component';
+import { UsuarioComponent } from './Components/usuario/usuario.component';
+import { ListarUsuarioComponent } from './Components/listar-usuario/listar-usuario.component';
+import { EliminarUsuarioComponent } from './Components/eliminar-usuario/eliminar-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    UsuarioComponent,
+    ListarUsuarioComponent,
+    EliminarUsuarioComponent
   ],
   imports: [
     BrowserModule,
