87e059b9f9cb78fb70ead3c679aa3c3466b45e5b
Carlos Andres <tasajos@gmail.com>
2023-02-16 14:41:27 -0400

    update controller

---
+++ Bok/B3ND/B3ND/Controllers/PersonalController.cs
@@ -0,0 +1,41 @@
+﻿using B3ND.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace B3ND.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonalController : ControllerBase
+    {
+        private readonly DBCN _context;
+
+        public PersonalController(DBCN context)
+        {
+
+
+        _context = context;
+
+        }
+
+        [HttpGet] 
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var listpersonal = await _context.Personales.ToListAsync();
+                return Ok(listpersonal);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+
+            
+        }
+
+        
+    }
+}
