572f2bc0e6cc2e65373d126c040748fa605a98d6
Carlos Azcarraga <tasajos@gmail.com>
2023-02-28 11:01:07 -0400

    shared module

---
+++ ProyN/src/app/Shared/shared.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+//Angular Modulos
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
+import {MatListModule} from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatListModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatIconModule,
+    MatTooltipModule
+   
+  ],
+  exports: [
+
+    MatSlideToggleModule,
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatListModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatIconModule,
+    MatTooltipModule
+   
+  ]
+})
+export class SharedModule { }
--- ProyN/src/app/app.module.ts
+++ ProyN/src/app/app.module.ts
@@ -8,22 +8,12 @@ import { PrincipalComponent } from './Components/principal/principal.component';
 import { UsuarioComponent } from './Components/usuario/usuario.component';
 import { ListarUsuarioComponent } from './Components/listar-usuario/listar-usuario.component';
 import { EliminarUsuarioComponent } from './Components/eliminar-usuario/eliminar-usuario.component';
-
-//Angular modulos
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatTableModule} from '@angular/material/table';
-import {MatListModule} from '@angular/material/list';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import { SharedModule } from './Shared/shared.module';
+
+//Angular modulos
+
 
 
 
@@ -40,19 +30,9 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule,
-    MatCardModule,
-    MatButtonModule,
-    MatGridListModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatTableModule,
-    MatListModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatIconModule,
-    MatTooltipModule
-   
+    SharedModule
+    
+
  
   ],
   providers: [
