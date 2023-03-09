0c0253e3b5844ee6001552490ba650ba05cc2255
Carlos Azcarraga <tasajos@gmail.com>
2023-03-09 18:35:14 -0400

    bootstrap

--- ProyN/src/app/app-routing.module.ts
+++ ProyN/src/app/app-routing.module.ts
@@ -2,20 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 //Componentes
 import { EliminarUsuarioComponent } from './Components/eliminar-usuario/eliminar-usuario.component';
+import { IULOGINComponent } from './Components/iulogin/iulogin.component';
 import { ListarUsuarioComponent } from './Components/listar-usuario/listar-usuario.component';
 import { PrincipalComponent } from './Components/principal/principal.component';
 import { UsuarioComponent } from './Components/usuario/usuario.component';
 import { VerUsuarioComponent } from './Components/ver-usuario/ver-usuario.component';
 
 const routes: Routes = [
-{path: '', redirectTo:'principal',pathMatch:'full'},
+//{path: '', redirectTo:'principal',pathMatch:'full'},
+{path: '', redirectTo:'uilogin',pathMatch:'full'},
 {path: 'principal',component:PrincipalComponent},
 {path: 'eliminar',component:EliminarUsuarioComponent},
 {path: 'listar',component:ListarUsuarioComponent},
 {path: 'usuarios',component:UsuarioComponent},
 {path: 'verusuarios/:id',component:VerUsuarioComponent},
 {path: 'editarusuarios/:id',component:UsuarioComponent},
-{path: '**', redirectTo: 'principal', pathMatch:'full'},
+{path: 'uilogin',component:IULOGINComponent},
+//{path: '**', redirectTo: 'principal', pathMatch:'full'},
+{path: '**', redirectTo: 'uilogin', pathMatch:'full'},
 ];
 
 @NgModule({
